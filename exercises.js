31607d702edde5a74a93cd835b1736d3c684869b
theresie <thereicyitegetse3@gmail.com>
2026-02-17 16:04:42 +0200

    first commit

---
+++ exercises.js
@@ -0,0 +1,10 @@
+//exercises 1
+
+let productCategories = ["Electronics", "Books", "Clothing", "Food", "Sports"];
+
+console.log(productCategories[0]); 
+console.log(productCategories[productCategories.length - 1]);
+console.log(productCategories.length);
+
+productCategories[1] = "Toys"; 
+console.log(productCategories);
