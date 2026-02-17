2a324bebc5835a6da60c513b973ae391ae03b60a
theresie <thereicyitegetse3@gmail.com>
2026-02-17 16:11:11 +0200

    improvement

--- exercises.js
+++ exercises.js
@@ -8,3 +8,22 @@ console.log(productCategories.length);
 
 productCategories[1] = "Toys"; 
 console.log(productCategories);
+
+//exercises 2
+
+let inventory = [10, 20, 30];
+
+inventory.push(40);
+console.log(inventory); 
+
+inventory.pop();
+console.log(inventory);
+
+inventory.unshift(0);
+console.log(inventory);
+
+inventory.shift();
+console.log(inventory); 
+
+inventory.splice(1, 1, 25, 35);
+console.log(inventory);
